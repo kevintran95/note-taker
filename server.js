26e2d7c30cd2b98d0d536bbe7e53671c50b1b648
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-14 20:53:32 -0700

    added routes folder

--- server.js
+++ server.js
@@ -16,4 +16,11 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+// Listen PORT
+app.listen(PORt, () => 
+    console.log(`listening at http://localhost:${PORT}`)
+);
